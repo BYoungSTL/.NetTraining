7665e15546134c0088676b498ae92570989f19d6
MaksimOleynik <maksimoleynik@coherentsolutions.com>
2021-10-22 14:31:45 +0300

    little style code fix

--- NET1/NET01/Entities/TrainingMaterials.cs
+++ NET1/NET01/Entities/TrainingMaterials.cs
@@ -5,14 +5,15 @@ namespace NET01.Entities
 {
     public abstract class TrainingMaterials : EntityID
     {
-        private string? _description; 
+        private string? _description;
+        private const int DescriptionLength = 256;
         public string MaterialType { get; set; }
         public string? Description
         {
             get => _description;
             init
             {
-                if (value != null && value.Length > 256)
+                if (value != null && value.Length > DescriptionLength)
                 {
                     throw new ArgumentException("Description is too long! (Max Length = 256)");
                 }
